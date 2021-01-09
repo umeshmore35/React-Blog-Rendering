0c2c305d6dc047d1c4df8f585a82dfac32ba0f6a
Trainee 29 <umesh.more@raweng.com>
2021-01-10 02:41:16 +0530

    added componetupdate in blog for links

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -9,6 +9,12 @@ class Blog extends Component {
     let id = this.props.match.params.id;
     this.props.findById(id);
   };
+  componentDidUpdate = (prevprops) => {
+    let id = this.props.match.params.id;
+    if (this.props.match.params.id !== prevprops.match.params.id) {
+      this.props.findById(id);
+    }
+  };
   render() {
     let { blog } = this.props;
 
