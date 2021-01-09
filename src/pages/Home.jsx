a6aca07206df1d468e2ef76fcc3c460b51c65bc2
Trainee 29 <umesh.more@raweng.com>
2021-01-09 13:51:49 +0530

    Added Routing and Pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
